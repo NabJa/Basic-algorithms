b6076a5ff3376d93d8b4d2e313b9651f5159ecea
NabJa <32510324+NabJa@users.noreply.github.com>
2018-04-25 17:17:49 +0200

    Add files via upload

--- smms.java
+++ smms.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.TreeSet;
 
@@ -14,21 +15,33 @@ public class smms {
 	static long startTime;
 	static long endTime;
 	static String out_path;
+
+	// Output txt file specification
 	static boolean o_spec = false;
-	static boolean empty = false;
 
-	static ArrayList<int[]> amss = new ArrayList<int[]>();
+	// If giveAmss is set to true, all amss are given back
 	static boolean giveAmss = true;
-
+	static ArrayList<int[]> amss = new ArrayList<int[]>();
 	static TreeSet<int[]> treeAmss = new TreeSet<int[]>(new arrayComp());
+
+	// Current max of divide and conquer algorithm
 	static int recMax = 0;
 
+	// static boolean empty = false;
+
+	// If measureTime is set to true, program outputs a txt with time measures fr
+	// different seq length
+	static boolean measureTime = true;
+	static randomSeqGenerator seqGen;
+
 	public static void main(String[] args) throws Exception {
+
+		// Cheack and read input
 		if (args[0].equals("-i")) {
 			readInput(args[1]);
 		} else {
 			System.err.println("Input path should be specified after \"-i\" tag");
-			System.err.print("Given coomand: ");
+			System.err.print("Given command: ");
 			for (String s : args) {
 				System.err.print(s + " ");
 			}
@@ -36,129 +49,135 @@ public class smms {
 		}
 
 		// Check if -o flag is given.
-		if (args.length > 3) {
-			out_path = args[4];
-			o_spec = true;
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals("-o")) {
+				o_spec = true;
+				out_path = args[i + 1];
+			}
+		}
+
+		if (measureTime) {
+			seqGen = new randomSeqGenerator();
 		}
 
 		for (int i = 0; i < args.length; i++) {
 
 			switch (args[i]) {
+			// Naive algorithm flag
 			case "-n":
-				startTime = System.nanoTime();
-				output = naive(seq);
-				endTime = System.nanoTime();
-				if (output[0] == 1 & output[1] == 0 & output[2] == 0) {
-					empty = true;
-				}
-				// System.out.print("Naive algorithm runtime in mikro seconds: ");
-				// System.out.println(Math.floor((endTime - startTime) / 1000));
-				if (!empty) {
-					if (o_spec) {
-						File out_file = new File(out_path, "smms.txt");
-						writeOutput(output, out_file, giveAmss);
-					} else {
-						if (giveAmss) {
-							for (int[] mss : amss) {
-								for (int ms : mss) {
-									System.out.print(ms + "\t");
-								}
-								System.out.println();
-							}
+				if (measureTime) {
+					writeTime(10, 1010, 10, 1000, "C:\\Users\\Anja\\Desktop", "naive_time.txt", "n");
+				} else {
+					output = naive(seq);
+					if (!(output[0] == 1 & output[1] == 0 & output[2] == 0)) {
+						if (o_spec) {
+							File out_file = new File(out_path, "smms.txt");
+							writeOutput(output, out_file, giveAmss);
 						} else {
-							for (int r : output) {
-								System.out.print(r + "\t");
+							if (giveAmss) {
+								for (int[] mss : amss) {
+									for (int ms : mss) {
+										System.out.print(ms + "\t");
+									}
+									System.out.println();
+								}
+							} else {
+								for (int r : output) {
+									System.out.print(r + "\t");
+								}
 							}
 						}
 					}
 				}
 				break;
 
+			// Linear algorithm flag
 			case "-l":
-				startTime = System.nanoTime();
-				output = linear(seq);
-				endTime = System.nanoTime();
-				if (output[0] == 1 & output[1] == 0 & output[2] == 0) {
-					empty = true;
-				}
-				// System.out.print("Linear algorithm runtime in mikro seconds: ");
-				// System.out.println(Math.floor((endTime - startTime) / 1000));
-				if (!empty) {
-					if (o_spec) {
-						File out_file = new File(out_path, "smms.txt");
-						writeOutput(output, out_file, giveAmss);
-					} else {
-						if (giveAmss) {
-							for (int[] mss : amss) {
-								for (int ms : mss) {
-									System.out.print(ms + "\t");
-								}
-								System.out.println();
-							}
+				if (measureTime) {
+					writeTime(10, 1010, 10, 1000, "C:\\Users\\Anja\\Desktop", "linear_time.txt", "l");
+				} else {
+					output = linear(seq);
+
+					if (!(output[0] == 1 & output[1] == 0 & output[2] == 0)) {
+						if (o_spec) {
+							File out_file = new File(out_path, "smms.txt");
+							writeOutput(output, out_file, giveAmss);
 						} else {
-							for (int r : output) {
-								System.out.print(r + "\t");
+							if (giveAmss) {
+								for (int[] mss : amss) {
+									for (int ms : mss) {
+										System.out.print(ms + "\t");
+									}
+									System.out.println();
+								}
+							} else {
+								for (int r : output) {
+									System.out.print(r + "\t");
+								}
 							}
 						}
 					}
 				}
+
 				break;
 
+			// Dynamic algorithm flag
 			case "-d":
-				startTime = System.nanoTime();
-				output = smms_dp(seq);
-				endTime = System.nanoTime();
-
-				if (output[0] == 1 & output[1] == 0 & output[2] == 0) {
-					empty = true;
-				}
-				// System.out.print("Dynamic algorithm runtime in mikro seconds: ");
-				// System.out.println(Math.floor((endTime - startTime) / 1000));
-				if (!empty) {
-					if (o_spec) {
-						File out_file = new File(out_path, "smms.txt");
-						writeOutput(output, out_file, giveAmss);
-					} else {
-						if (giveAmss) {
-							for (int[] mss : amss) {
-								for (int ms : mss) {
-									System.out.print(ms + "\t");
-								}
-								System.out.println();
-							}
+				if (measureTime) {
+					writeTime(10, 1010, 10, 1000, "C:\\Users\\Anja\\Desktop", "dynamic_time.txt", "d");
+				} else {
+					output = smms_dp(seq);
+					if (!(output[0] == 1 & output[1] == 0 & output[2] == 0)) {
+						if (o_spec) {
+							File out_file = new File(out_path, "smms.txt");
+							writeOutput(output, out_file, giveAmss);
 						} else {
-							for (int r : output) {
-								System.out.print(r + "\t");
+							if (giveAmss) {
+								for (int[] mss : amss) {
+									for (int ms : mss) {
+										System.out.print(ms + "\t");
+									}
+									System.out.println();
+								}
+							} else {
+								for (int r : output) {
+									System.out.print(r + "\t");
+								}
 							}
 						}
 					}
 				}
 				break;
 
+			// Divide&Conquer algorithm flag
 			case "-dc":
-				startTime = System.nanoTime();
-				output = mms_dc(seq);
-				endTime = System.nanoTime();
-
-				if (output[0] == 0 & output[1] == -1 & output[2] == 0) {
-					empty = true;
-				}
-				// System.out.print("Divide&Conquer algorithm runtime in mikro seconds: ");
-				// System.out.println(Math.floor((endTime - startTime) / 1000));
-				if (!empty) {
-					if (o_spec) {
-						File out_file = new File(out_path, "smms.txt");
-						writeOutput(output, out_file, giveAmss);
-					} else {
-						for (int r : output) {
-							System.out.print(r + "\t");
+				if (measureTime) {
+					writeTime(10, 1010, 10, 1000, "C:\\Users\\Anja\\Desktop", "dc_time.txt", "dc");
+				} else {
+					output = mms_dc(seq);
+					if (!(output[0] == 0 & output[1] == -1 & output[2] == 0)) {
+						if (o_spec) {
+							File out_file = new File(out_path, "smms.txt");
+							writeOutput(output, out_file, giveAmss);
+						} else {
+							if (giveAmss) {
+								for (int[] mss : treeAmss) {
+									for (int ms : mss) {
+										System.out.print(ms + "\t");
+									}
+									System.out.println();
+								}
+							} else {
+								for (int r : output) {
+									System.out.print(r + "\t");
+								}
+							}
 						}
 					}
 				}
 				break;
 			}
 		}
-
 	}
 
 	/**
@@ -228,6 +247,68 @@ public class smms {
 	}
 
 	/**
+	 * Measures time of specified algorithm over specified time with specified
+	 * accuracy.
+	 * 
+	 * @param seqMin
+	 * @param seqMax
+	 * @param step
+	 * @param accuracy
+	 * @param path
+	 * @param fileName
+	 * @param algorithm
+	 */
+	public static void writeTime(int seqMin, int seqMax, int step, int accuracy, String path, String fileName,
+			String algorithm) {
+		ArrayList<Integer> times = new ArrayList<Integer>();
+		int[] out;
+		for (int j = seqMin; j < seqMax; j += step) {
+			int[] seq = seqGen.generateSequence(j, -9, 9);
+			long startT = System.nanoTime();
+			switch (algorithm) {
+			case "n":
+				out = naive(seq);
+				break;
+			case "d":
+				out = smms_dp(seq);
+				break;
+			case "dc":
+				out = mms_dc(seq);
+				break;
+			case "l":
+				out = linear(seq);
+				break;
+			}
+			long endT = System.nanoTime();
+			int time = (int) Math.floor((endT - startT) / accuracy);
+			times.add(time);
+		}
+		File out_file_time = new File(path, fileName);
+		writeTimesOutput(times, out_file_time);
+	}
+
+	/**
+	 * Writes output times.
+	 * 
+	 * @param result
+	 * @param path
+	 */
+	public static void writeTimesOutput(ArrayList<Integer> times, File file) {
+		try {
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter writer = new BufferedWriter(fw);
+
+			for (Integer time : times) {
+				writer.write(time.toString());
+				writer.newLine();
+			}
+			writer.close();
+		} catch (Exception e) {
+			throw new RuntimeException("Got error while writing output", e); // TODO handle exception properly
+		}
+	}
+
+	/**
 	 * Naive algorithm
 	 * 
 	 * @param a
@@ -252,7 +333,8 @@ public class smms {
 					l = i - 1;
 					r = j - 1;
 					if (giveAmss) {
-						// Remove everything in amms list and add new max if new max is bigger then previous max
+						// Remove everything in amms list and add new max if new max is bigger then
+						// previous max
 						amss.removeAll(amss);
 						int[] mss = { l, r, maxscore };
 						amss.add(mss);
@@ -364,17 +446,16 @@ public class smms {
 			if (rmax > 0) {
 				rmax = rmax + a[i];
 				frmax = frmax + a[i];
-			} else if (rmax == 0) {
+			} else if (rmax == 0 & i != 0) {
+				frstart = rstart;
 				frmax = frmax + a[i];
-				rmax = a[i];
-				frstart = i;
 				rstart = i;
-
+				rmax = a[i];
 			} else {
-				frmax = a[i];
-				frstart = i;
-				rmax = a[i];
 				rstart = i;
+				rmax = a[i];
+				frstart = i;
+				frmax = a[i];
 			}
 
 			if (giveAmss & rmax > max) {
@@ -431,16 +512,6 @@ public class smms {
 	public static int[] mms_dc(int[] a) {
 		int n = a.length;
 		int[] result = mms_dc_rec(0, n - 1, a);
-
-		for (int[] mss : treeAmss) {
-			for (int ms : mss) {
-				System.out.print(ms + " ");
-			}
-			System.out.println();
-		}
-		
-		System.out.println(treeAmss.contains(result));
-		
 		return result;
 	}
 
@@ -474,11 +545,19 @@ public class smms {
 			int s = a[i3];
 			int simax = s;
 
+			int fsimax = s;
+			int fi3 = m;
+			ArrayList<int[]> midmaxs = new ArrayList<int[]>();
+
 			for (int k = i3 - 1; k >= i; k--) {
 				s = s + a[k];
 				if (s > simax) {
 					simax = s;
 					i3 = k;
+				}
+				if (giveAmss & s == simax) {
+					fsimax = s;
+					fi3 = k;
 				}
 			}
 
@@ -487,48 +566,47 @@ public class smms {
 			s = a[j3];
 			int sjmax = s;
 
+			int fsjmax = s;
+			int fj3 = m + 1;
+
 			for (int k = j3 + 1; k <= j; k++) {
 				s = s + a[k];
 				if (s > sjmax) {
 					sjmax = s;
 					j3 = k;
 				}
+				if (giveAmss & s == sjmax) {
+					fsjmax = s;
+					fj3 = k;
+					int s4 = fsimax + fsjmax;
+					int[] fjmax = { fi3, fj3, s4 };
+					midmaxs.add(fjmax);
+				}
 			}
 
 			// Get total max
 			int s3 = simax + sjmax;
+			int[] midSeq = { i3, j3, s3 };
 
-//				if(	treeAmss.ceiling(preSeq) == null) {
-//					treeAmss.clear();
-//					treeAmss.add(preSeq);
-//				} else if(treeAmss.contains(preSeq)) {
-//					treeAmss.add(preSeq);	
-//				}
-//			
-//				int[] midSeq = {i3, j3, s3 };
-//				if(	treeAmss.ceiling(midSeq) == null) {
-//					treeAmss.clear();
-//					treeAmss.add(midSeq);
-//				} else if(treeAmss.contains(midSeq)) {
-//					treeAmss.add(midSeq);
-//				}
-//			
-//				if(	treeAmss.ceiling(postSeq) == null) {
-//					treeAmss.clear();
-//					treeAmss.add(postSeq);
-//				} else if(treeAmss.contains(postSeq)) {
-//					treeAmss.add(postSeq);
-//				}
-			
+			if (giveAmss) {
+				int s4 = fsimax + fsjmax;
+				int[] fmidSeq = { fi3, fj3, s4 };
+				for (int[] midmax : midmaxs) {
+					addToTreeList(midmax);
+				}
+				addToTreeList(fmidSeq);
+				addToTreeList(preSeq);
+				addToTreeList(midSeq);
+				addToTreeList(postSeq);
+			}
 
 			// PreSeq contains sequence with highest score?
 			if (preSeq[2] >= postSeq[2] & preSeq[2] >= s3) {
 				// postSeq has same score and is shorter?
 				if (preSeq[2] == postSeq[2] & preSeq[1] - preSeq[0] > postSeq[1] - postSeq[0]) {
-					return postSeq;	
+					return postSeq;
 					// midSeq has same score and is shorter?
 				} else if (preSeq[2] == s3 & preSeq[1] - preSeq[0] > j3 - i3) {
-					 int[] midSeq = { i3, j3, s3 };
 					return midSeq;
 				} else {
 					return preSeq;
@@ -539,7 +617,6 @@ public class smms {
 				if (s3 == postSeq[2] & postSeq[1] - postSeq[0] < j3 - i3) {
 					return postSeq;
 				} else {
-					 int[] midSeq = { i3, j3, s3 };
 					return midSeq;
 				}
 			} else {
@@ -547,19 +624,37 @@ public class smms {
 			}
 		}
 	}
-}
 
-class arrayComp implements Comparator<int[]>{
-	@Override
-	public int compare(int[] a, int[] b) {
-		if(a[2] < b[2]) {
-			return -1;
-		} else if(a[2] > b[2]) {
-			return 1;
-		} else {
-			return 0;
+	/**
+	 * adds int[3] sequence to treeSet if score >= previous score
+	 * 
+	 * @param seq
+	 */
+	public static void addToTreeList(int[] seq) {
+		if (seq[2] > recMax) {
+			recMax = seq[2];
+			treeAmss.removeAll(treeAmss);
+			treeAmss.add(seq);
+		} else if (seq[2] == recMax) {
+			treeAmss.add(seq);
 		}
 	}
-	
-	
+}
+
+class arrayComp implements Comparator<int[]> {
+	@Override
+	public int compare(int[] a, int[] b) {
+		if (a[0] == b[0] & a[1] == b[1]) {
+			return 0;
+		} else if (a[0] < b[0]) {
+			return -1;
+		} else if (a[0] > b[0]) {
+			return 1;
+		} else if (a[1] < b[1]) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+
 }
